3150f8ffd7426d2f96a63aac0b23a6f7cfc59648
Wall <ilnazamirhanov61@gmail.com>
2023-01-10 23:43:19 +0300

    add routes for logout system

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,4 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome');
+Route::name('user.')->group(function (){
+    Route::view('private','private')->middleware('auth')->name('private');
+    Route::get('/login', function () {
+        if (\Illuminate\Support\Facades\Auth::check()) {
+            return redirect(\route('user.private'));
+    }
+        return view('login');
+    })->name('login');
+   //Route::post('/login', [])
+    //Route::get('/logout', [])->name('logout');
+    Route::get('/registation', function (){
+        if (\Illuminate\Support\Facades\Auth::check()) {
+            return redirect(\route('user.private'));
+        }
+        return view('registation');
+    })->name('registation');
+});
